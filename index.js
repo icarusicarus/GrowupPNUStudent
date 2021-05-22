f9ab0e470e0d89cb5247a73a25c562cb548ea181
pipisebastian <yrt7998@pusan.ac.kr>
2021-05-23 01:11:38 +0900

    Update routing

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 var mainRouter = require("./router/main");
-var choiceRouter = require("./router/choice");
+var NewOrContinueRouter = require("./router/NewOrContinue");
+var SignUpRouter = require("./router/SignUp");
+var tutorialRouter = require("./router/tutorial");
+var MainPageRouter = require("./router/MainPage");
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/',mainRouter);
-app.use('/choice',choiceRouter);
+app.use('/NewOrContinue',NewOrContinueRouter);
+app.use('/SignUp',SignUpRouter);
+app.use('/tutorial',tutorialRouter);
+app.use('/MainPage',MainPageRouter);
 
 app.listen(app.get('port'),function(){
     console.log('[Listening] localhost @',app.get('port'));
--- router/SignUp.js
+++ router/SignUp.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',async function(req,res){
-    console.log('[main.js] Main page loading...');
-    res.render('main');
+    res.render('SignUp');
 });
-
 
 module.exports = router;
